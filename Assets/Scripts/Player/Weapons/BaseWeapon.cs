0c3cb092087d11c0c05fd43738e0f9970e7be95c
daanfranke <daanfranke@gmail.com>
2025-09-09 17:11:46 +0200

    Made base weapons class abstract

--- Assets/Scripts/Player/Weapons/BaseWeapon.cs
+++ Assets/Scripts/Player/Weapons/BaseWeapon.cs
@@ -1,7 +1,7 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-public class BaseWeapon : MonoBehaviour
+public abstract class BaseWeapon : MonoBehaviour
 {
     public WeaponStats weaponStats;
     public bool outOfStamina;
